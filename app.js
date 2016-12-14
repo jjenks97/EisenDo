c600a078bb02e4bb7c72a0fdc62bc9eafc768e38
jjenks97 <jonathan.jenkins97@gmail.com>
2016-12-14 02:54:16 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,17 +1,30 @@
 /*jslint browser: true*/
 /*global $, jQuery, alert*/
 
-document.getElementById("add").onclick = function () {
-    var toDoTask = document.getElementById("toDo").value;
-    var listItem = document.createElement('li');
-    listItem.innerHTML = toDoTask;
-    document.getElementById("sortable1").appendChild(listItem);
-    listItem.setAttribute("class", "ui-state-default");
-};
+$(function () {
 
+    $('#add').on("click", function () {
+        var toDoTask = document.getElementById("toDo").value;
+        var listItem = document.createElement('li');
+        var button = document.createElement("button");
+        button.innerHTML = "&#x2713";
+        listItem.innerHTML = toDoTask;
+        document.getElementById("sortable1").appendChild(listItem);
+        listItem.setAttribute("class", "ui-state-default");
+        listItem.setAttribute("id", "listTask");
+        listItem.appendChild(button);
+        button.setAttribute("id", "completeTaskButton");
+        button.setAttribute("class", "completeTaskButton");
+    });
+    $('#completeTaskButton').on("click", function () {
+        $(this).parent().remove();
+    });
+});
+
+    
 
 $(function () {
-    $("#sortable1, #sortable2").sortable({
+    $("#sortable1, #sortable2, #sortable3, #sortable4, #sortable5").sortable({
         connectWith: ".connectedSortable"
     }).disableSelection();
 });
